bf2064d671330c33fcac9531c8b94275d6004973
whcheungav <marcocheung0521@gmail.com>
2025-11-18 15:55:21 +0800

    Update expense_tracker.py

--- expense_tracker.py
+++ expense_tracker.py
@@ -14,16 +14,16 @@ with st.form("Expense_Form"):
     amount=st.number_input("Amount",min_value=0.0,step=0.01)
     description=st.text_input("Description")
 
-submitted=st.form_submit_button("Add Expense")
-if submitted:
-    new_expense=pd.DataFrame({
-     'Date':[date],
-     'Category':[category],
-     'Amount':[amount],
-     'Description':[description]
-})
-    st.session_state.expenses=pd.concat([st.session_state.expenses, new_expense],ignore_index=True)
-    st.success("Expense added successfully!")
+    submitted=st.form_submit_button("Add Expense")
+    if submitted:
+        new_expense=pd.DataFrame({
+         'Date':[date],
+         'Category':[category],
+         'Amount':[amount],
+         'Description':[description]
+    })
+        st.session_state.expenses=pd.concat([st.session_state.expenses, new_expense],ignore_index=True)
+        st.success("Expense added successfully!")
 if not st.session_state.expenses.empty:
     st.subheader("Your Expenses")
     st.dataframe(st.session_state.expenses)
